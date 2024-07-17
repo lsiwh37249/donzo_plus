e13567aa23c950a0ba0cd09fd0f9317ad6cf6c99
lsiwh37249 <lsiwh37249@gmail.com>
2024-07-17 11:15:22 +0900

    update func

--- src/donzo_plus/cal_plus.py
+++ src/donzo_plus/cal_plus.py
@@ -1,7 +1,13 @@
 import sys
 
-a = int(sys.argv[1])
-b = int(sys.argv[2])
+def plus(a,b):
+    print(a+b)
 
-print(a+b)
+def main():
 
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+
+    plus(a,b)
+if __name__ == "__main__":
+    main()
